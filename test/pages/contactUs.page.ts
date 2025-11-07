dde2c509bd16bd84474c815ccaba10ac30cdd926
Mykhail <its.astral.step@gmail.com>
2025-11-07 18:18:00 +0200

    project structure and configs

---
+++ test/pages/contactUs.page.ts
@@ -0,0 +1,30 @@
+import endpoints from '../constants/endpoints.constants.json'
+import BasePage from "./base.page"
+
+
+class ContactUsPage extends BasePage {
+    endpoint = endpoints.contactUs
+
+    get selectDropdown () {return $('select[name="Reason_for_Contact__c"]')}
+    get firstnameInput () {return $('input[name="FirstName"]')}
+    get lastnameInput () {return $('input[name="LastName"]')}
+    get emailInput () {return $('input[name="Email"]')}
+    get phoneInput ()  {return $('input[name="Phone_Number_Base__c"]')}
+    get websiteInput ()  {return $('input[name="Website"]')}
+    get additionalInfoInput () {return $('textarea[name="Form_Additional_Information__c"]')}
+    get hearAboutInput () {return $('input[name="How_did_you_hear_about_Telnyx_Open__c"]')}
+    get submitButton () {return $('button[type="submit"]')}
+    get invalidDropdownSelectMessage () {return $('#ValidMsgReason_for_Contact__c')}
+    get invalidEmailMessage () {return}
+    get reportAbuseButton () {return}
+
+
+    async submitButtonClick() {
+        await this.submitButton.click()
+    }
+
+    async open() {
+        await super.open(this.endpoint)
+    }
+}
+export default ContactUsPage
